ba8e8c5cdb8e287731dcd3eeab262bf597eae61a
Andres Briñez <brinezlopez08@gmail.com>
2024-08-08 09:55:12 -0500

    Se agregan algunas notas

--- 01-Fundamentos/src/queryResolvers.ts
+++ 01-Fundamentos/src/queryResolvers.ts
@@ -3,6 +3,11 @@
 
 // Tipo Query, se debe definir el tipado
 // por estandar se llama typeDefs, esto es lo que se le manda a apollo server
+
+/* Definición del Schema (typeDefs): Se define la estructura de los datos usando GraphQL Schema Language. Aquí se declaran dos tipos:
+  User: Representa un usuario con campos 'name' y 'email'.
+  Query: Define las consultas disponibles, en este caso 'users'.
+ */
 const typeDefs = `#graphql 
 
   type User{
@@ -17,28 +22,30 @@ const typeDefs = `#graphql
 `;
 
 // DataSet, fuente de informacion que se puede recibir de una base de datos
-const users =[
+// Datos de muestra (users): Se crea un array con datos de usuario para simular una fuente de datos
+const users = [
   {
-    name:"Andres",
-    email:"andres@gmail.com",
+    name: "Andres",
+    email: "andres@gmail.com",
 
   },
   {
-    name:"Felipe",
-    email:"felipe@gmail.com",
+    name: "Felipe",
+    email: "felipe@gmail.com",
 
   },
   {
-    name:"Briñez",
-    email:"briñez@gmail.com",
+    name: "Briñez",
+    email: "briñez@gmail.com",
 
   },
 
 ]
 
-// Resolver
-const resolvers={
-  Query:{
-    users:()=>users[0],
+// Resolvers: Funciones que determinan cómo se obtienen los datos para cada campo.
+// Se define cómo se resuelven las consultas. En este caso, la consulta 'users' devuelve el primer usuario del array.
+const resolvers = {
+  Query: {
+    users: () => users[0],
   }
 }
